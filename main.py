5997455fb2b63301be0b1a80924889111b41c854
nenad-misic <33166172+nenad-misic@users.noreply.github.com>
2021-07-04 21:17:10 +0200

    Frontend devops (#24)
    * Refactor code to be flask independent and add message broker implementation
    
    * Fix failing build
    
    * Fix failing build
    
    * Fix failing build 3
    
    * God damn environment
    
    * Add docker-compose for frontend closes #18
    
    * Exclude html content from sc

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from os import environ
 from common.config import setup_config
+from flask import send_from_directory
+import os
+app = setup_config('dev')
+
+@app.route('/')
+def send_index():
+    return send_from_directory('../public/components/index/', 'index.html')
+
+@app.route('/static/<path:path>')
+def send(path):
+    return send_from_directory('../public', path)
+
 
 if __name__ == '__main__':
-    app = setup_config('dev')
     app.run(host=environ.get('FLASK_RUN_HOST'))
